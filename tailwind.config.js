36005bc83e8af93bbeb8f0e7c1e05a005a52e8a5
Hamza Altamas <hamza.altamas.01876@gmail.com>
2022-09-11 22:51:20 +0600

    syntax and navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        primarybg: "#F2F2E7",
+        blue: "#203239",
+      },
+      fontFamily: {
+        pop: "Poppins",
+        robo: "Roboto",
+      },
+      maxWidth: {
+        container: "1100px",
+      },
+    },
+  },
+};
